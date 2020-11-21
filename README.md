43acc69fb6add56a5f55252e4bf2ccaf9fe1711b
Vagelis Prokopiou <vagelis.prokopiou@gmail.com>
2020-11-21 17:54:48 +0200

    Replacing sassc with Dart Sass and updating the README
    Signed-off-by: Vagelis Prokopiou <vagelis.prokopiou@gmail.com>

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # sass2css
-
 After having struggled for many years with the Node.js (node_modules) hell, if decided to create this alternative for my frontend theming workflow.
+Currently, it includes sass to css compilation (with [Dart Sass](https://github.com/sass/dart-sass)) and browser syncing on file change (replacing [Browsersync](https://www.browsersync.io)).
 
-Currently, it includes sass to css compilation (with [SassC](https://github.com/sass/sassc)) and browser syncing on file change (replacing [Browsersync](https://www.browsersync.io)).
-
-Feel free to use it to your liking.
+## Usage
+* Install the [Dart Sass](https://github.com/sass/dart-sass) standalone executable.
+* Download the script in your theme folder.
+* Make it executable: `chmod +x ./sass2css.sh`
+* Update the configuration variables according to your setup.
+* Run it with: `./sass2css.sh`
--- sass2css.sh
+++ sass2css.sh
@@ -6,24 +6,24 @@
 
 command -v inotifywait >/dev/null 2>&1 || sudo apt install -y inotify-tools
 command -v xdotool >/dev/null 2>&1 || sudo apt install -y xdotool
-command -v sassc >/dev/null 2>&1 || { echo -e "\nsassc is required but it is not installed.\nInstall from https://github.com/sass/sassc/blob/master/docs/building/unix-instructions.md\n" && exit 1; }
+command -v sass >/dev/null 2>&1 || { echo -e "\ndart-sass is required but it is not installed.\nInstall the latest Linux version from $(curl -s "https://api.github.com/repos/sass/dart-sass/releases/latest" | grep download_url | grep linux-x64 | awk '{print $2}')\n" && exit 1; }
 
-# Variables.
-sass_source_dir="./style/scss"
-sass_source_file="./style/scss/main.scss"
-css_target_file="./style/css/main.css"
-css_style="compressed"              # Can be: nested or expanded or compact or compressed.
-sourcemap_type="auto"               # Can be: auto or inline.
+
+# Configuration variables.
+sass_source_dir="./src/scss"
+sass_source_file="./src/scss/style.scss"
+css_target_file="./css/style.css"
+css_style="compressed"              # Can be: expanded or compressed.
 type_of_browser_reload="ctrl+F5"    # Can be: ctrl+F5 or ctrl+r
-window_to_restore_to="drupal.*scss" # You have to figure this out fro your kind of setup
-browsers="Firefox"                  # Can add multiple. E.g.: browsers="Chrome Firefox Brave"
+window_to_restore_to="style.scss"   # You have to figure this out fro your kind of setup
+browsers="Brave"                    # Can add multiple. E.g.: browsers="Chrome Firefox Brave"
 
 # Make an initial build.
-sassc --sourcemap="${sourcemap_type}" --style "${css_style}" "${sass_source_file}" "${css_target_file}"
+sass --update --style="${css_style}" "${sass_source_file}" "${css_target_file}"
 
 while inotifywait --recursive -e modify "${sass_source_dir}"; do
   #  Compile sass.
-  sassc --sourcemap="${sourcemap_type}" --style "${css_style}" "${sass_source_file}" "${css_target_file}"
+  sass --update --style "${css_style}" "${sass_source_file}" "${css_target_file}"
 
   #Reload the browser.
   for browser in ${browsers}; do
